d2666f5fe497af5674a5826dcc5f1c99d3ecacf7
Mike <ovyan13@edu.hse.ru>
2020-05-10 20:58:49 +0300

    coloring path

--- graphWWDC/MyPlayground.playground/Contents.swift
+++ graphWWDC/MyPlayground.playground/Contents.swift
@@ -3,11 +3,15 @@
 import PlaygroundSupport
 import UIKit
 
-func onMain(_ block: @escaping() -> Void) {
+func onMain(_ block: @escaping () -> Void) {
     DispatchQueue.main.async(execute: block)
 }
 
 class MyViewController: UIViewController {
+    let contentLayer = CALayer()
+    let contentLayer2 = CALayer()
+    let contentLayerFinal = CALayer()
+
     let Adj_Matr = [
         [-1, 4, 3, -1, -1, -1, -1],
         [-1, -1, -1, 2, -1, -1, -1],
@@ -28,40 +32,113 @@ class MyViewController: UIViewController {
             print(cost[6])
             print(path)
             shortestPath = path
+            showFinalPath(path: shortestPath)
             return
         }
         for i in 0 ..< Adj_Matr[vert].count {
-            if Adj_Matr[vert][i] != -1, cost[i] == -1 || cost[vert] + Adj_Matr[vert][i] < cost[i] {
+            if Adj_Matr[vert][i] != -1, cost[i] == -1 || cost[vert] + Adj_Matr[vert][i] <= cost[i] {
                 cost[i] = cost[vert] + Adj_Matr[vert][i]
                 path.append(i)
+                self.showPath(path: self.path)
                 dfs(vert: i)
                 path.popLast()
+                self.showPath(path: self.path)
+            }
+        }
+    }
+
+    func showFinalPath(path: [Int]) {
+        onMain {
+            CATransaction.begin()
+            CATransaction.setDisableActions(true)
+            self.contentLayerFinal.sublayers?.forEach { $0.removeFromSuperlayer() }
+            CATransaction.commit()
+        }
+        for i in 0 ..< path.count - 1 {
+            onMain {
+                let startId = path[i] + 1
+                let endId = path[i + 1] + 1
+                let startCircle: UIView = self.view.viewWithTag(startId)! as UIView
+                let endCircle: UIView = self.view.viewWithTag(endId)! as UIView
+
+                let line = UIBezierPath()
+                line.move(to: CGPoint(x: startCircle.frame.midX, y: startCircle.frame.midY))
+                line.addLine(to: CGPoint(x: endCircle.frame.midX, y: endCircle.frame.midY))
+
+                let shapeLayer = CAShapeLayer()
+                shapeLayer.path = line.cgPath
+                shapeLayer.strokeColor = UIColor.green.cgColor
+                shapeLayer.lineWidth = 18
+
+                let circle = UIBezierPath(roundedRect: CGRect(x: endCircle.frame.midX - 10, y: endCircle.frame.midY - 10, width: 20, height: 20), cornerRadius: 10)
+
+                let shapeLayerCircle = CAShapeLayer()
+                shapeLayerCircle.path = circle.cgPath
+                shapeLayerCircle.strokeColor = UIColor.green.cgColor
+                shapeLayerCircle.lineWidth = 20
+
+                CATransaction.begin()
+                CATransaction.setDisableActions(true)
+                self.contentLayerFinal.addSublayer(shapeLayer)
+                self.contentLayerFinal.addSublayer(shapeLayerCircle)
+                CATransaction.commit()
             }
         }
     }
 
     func showPath(path: [Int]) {
-        for nodeId in path {
+        for i in 0 ..< path.count - 1 {
             onMain {
-                let my_circle: UIView = self.view.viewWithTag(nodeId + 1)! as UIView
-                my_circle.backgroundColor = UIColor.red
+                let startId = path[i] + 1
+                let endId = path[i + 1] + 1
+                let startCircle: UIView = self.view.viewWithTag(startId)! as UIView
+                let endCircle: UIView = self.view.viewWithTag(endId)! as UIView
+
+                let line = UIBezierPath()
+                line.move(to: CGPoint(x: startCircle.frame.midX, y: startCircle.frame.midY))
+                line.addLine(to: CGPoint(x: endCircle.frame.midX, y: endCircle.frame.midY))
+
+                let shapeLayer = CAShapeLayer()
+                shapeLayer.path = line.cgPath
+                shapeLayer.strokeColor = UIColor.white.cgColor
+                shapeLayer.lineWidth = 10
+
+                let circle = UIBezierPath(roundedRect: CGRect(x: endCircle.frame.midX - 5, y: endCircle.frame.midY - 5, width: 10, height: 10), cornerRadius: 5)
+
+                let shapeLayerCircle = CAShapeLayer()
+                shapeLayerCircle.path = circle.cgPath
+                shapeLayerCircle.strokeColor = UIColor.white.cgColor
+                shapeLayerCircle.lineWidth = 10
+
+                if path.count % 2 == 0 {
+                    self.contentLayer.addSublayer(shapeLayer)
+                    self.contentLayer.addSublayer(shapeLayerCircle)
+                } else {
+                    self.contentLayer2.addSublayer(shapeLayer)
+                    self.contentLayer2.addSublayer(shapeLayerCircle)
+                }
             }
-            
-            sleep(1)
         }
-        
+        usleep(300 * 1_000)
+        onMain {
+            CATransaction.begin()
+            CATransaction.setDisableActions(true)
+            if path.count % 2 == 1 {
+                self.contentLayer.sublayers?.forEach { $0.removeFromSuperlayer() }
+            } else {
+                self.contentLayer2.sublayers?.forEach { $0.removeFromSuperlayer() }
+            }
+            CATransaction.commit()
+        }
     }
 
-    let dx = [1, 5, 1, 5, 1, 5, 3]
-    let dy = [0, 0, 1, 1, 2, 2, 3]
+    let dx = [1, 9, 1, 9, 1, 9, 5]
+    let dy = [0, 0, 2, 2, 4, 4, 5]
 
     @objc
     func startTapped() {
-        dfs(vert: 0)
         DispatchQueue.global().async {
-            for i in 0...1 {
-                self.showPath(path: self.shortestPath)
-            }
+            self.dfs(vert: 0)
         }
     }
 
@@ -72,9 +149,10 @@ class MyViewController: UIViewController {
         var circles = [UIView]()
 
         for i in 0 ... 6 {
-            let circle = UIView(frame: CGRect(x: 60 * dx[i], y: 80 + 150 * dy[i], width: 40, height: 40))
-            circle.backgroundColor = UIColor.blue
-            circle.layer.cornerRadius = 20
+            let circle = UIView(frame: CGRect(x: 38 * dx[i], y: 42 + 105 * dy[i], width: 72, height: 72))
+            circle.backgroundColor = UIColor.systemBlue
+            circle.layer.cornerRadius = 35
+            circle.alpha = 0.8
             circle.tag = i + 1
 
             circles.append(circle)
@@ -89,19 +167,29 @@ class MyViewController: UIViewController {
         btn.addTarget(self, action: #selector(startTapped), for: .touchUpInside)
 
         view.addSubview(btn)
-        view.bounds.size.width = 100
-        view.bounds.size.height = 100
-        
+        view.layer.addSublayer(contentLayerFinal)
+        view.layer.addSublayer(contentLayer)
+        view.layer.addSublayer(contentLayer2)
+
+        view.bounds.size.height = 750
+        view.bounds.size.width = 450
+        view.layer.contents = #imageLiteral(resourceName: "field1.png").cgImage
+
         self.view = view
-        
     }
-    override func viewDidLayoutSubviews() {
-        print(view.bounds.size)
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        contentLayerFinal.bounds = view.layer.bounds
+        contentLayerFinal.position = view.center
+        contentLayer.bounds = view.layer.bounds
+        contentLayer.position = view.center
+        contentLayer2.bounds = view.layer.bounds
+        contentLayer2.position = view.center
     }
 }
 
 // Present the view controller in the Live View window
 let a = MyViewController()
-a.preferredContentSize = CGSize(width: 560, height: 880)
+a.preferredContentSize = a.view.frame.size
 PlaygroundPage.current.liveView = a
-
